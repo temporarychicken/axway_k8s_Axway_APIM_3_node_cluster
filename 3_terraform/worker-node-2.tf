af93ce85a61179b5e6df6ce871386d165986d7b3
temporarychicken <david.luke@yahoo.co.uk>
2022-01-21 17:17:01 +0000

    Added deployment option for Amplify Streams using Helm

--- 3_terraform/worker-node-2.tf
+++ 3_terraform/worker-node-2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "kubernetes0004-worker-node-2" {
   ami                         = data.aws_ami.k8s-base-machine.id # eu-west-2
-  instance_type               = "t2.large"
+  instance_type               = "t2.xlarge"
   key_name                    = "k8s-server-key-kubernetes0004"
   associate_public_ip_address = true
   security_groups             = [aws_security_group.nginx-web-facing.id]
